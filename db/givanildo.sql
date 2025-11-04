8d5622050f9fbbe5765f48dd5d88fee698759e9b
RafaelGonçalves <83875972+RafaelGoncalves-bit@users.noreply.github.com>
2025-11-04 19:17:59 -0300

    Create categorias table and insertion procedure
    Added categorias table and stored procedure for insertion.

--- db/givanildo.sql
+++ db/givanildo.sql
@@ -2,7 +2,15 @@ create database givanildo;
 
 use givanildo;
 
-create table tarefas(
+create table categorias(
     id int primary key auto_increment,
     nome VARCHAR(255) not null
 );
+
+delimiter $$
+CREATE OR replace PROCEDURE inserir_categoria(b_nome VARCHAR(255))
+BEGIN 
+INSERT INTO categorias (nome) VALUES (b_nome);
+
+END $$
+delimiter ;
